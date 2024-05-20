bbde9cf57db06ef28104f5a815bf51d75de980a2
Vlad <427departament@gmail.com>
2024-05-20 17:01:11 +0300

    Modify filename

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ sudo cp -f ./80-usb.rules /etc/udev/rules.d/80-usb.rules
 sudo udevadm control --reload-rules
 
 
-sudo cp -f ./screen-lock.service /etc/systemd/system/screen-lock.service
-sudo systemctl start screen-lock.service
-sudo systemctl stop screen-lock.service
+sudo cp -f ./screen_lock.service /etc/systemd/system/screen_lock.service
+sudo systemctl start screen_lock.service
+sudo systemctl stop screen_lock.service
 
 
-sudo cp -f ./screen-lock.sh /usr/local/bin/screen-lock.sh
+sudo cp -f ./screen_lock.sh /usr/local/bin/screen_lock.sh
 
 echo "End install"
